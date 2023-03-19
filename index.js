cf700f491b442da3312a15fa7e60fcfd5923e952
rknm-cell <racknahm@gmail.com>
2023-03-18 20:23:31 -0400

    Added scuber functions

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(nums){
+  let result
+  if (nums < 400){
+    return"This one is on me!"
+  } else if (nums > 400 && nums < 2000){
+    return "That will be twenty bucks."
+  } else if (nums > 2000 && nums < 2500){
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(location){
   // Write your code here!
-}
+  
+  return (location == "NYC" ? "Ok, sounds good." : "No go.")
+};
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch(tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  };
+};
